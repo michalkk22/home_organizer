e4a7ef6cac0056fb6d55885d461b7e1864205850
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-10-31 16:24:12 +0100

    added auth exceptions

---
+++ lib/features/auth/auth_exception.dart
@@ -0,0 +1,15 @@
+class InvalidEmailAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class NewtowrkFailAuthException implements Exception {}
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
