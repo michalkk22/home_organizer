60b8430a51f7ac326d3d18dd881fce258ea414aa
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-11-13 13:16:49 +0100

    added tab bar navigation to home page

--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -2,28 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_organizer/features/auth/bloc/auth_bloc.dart';
 import 'package:home_organizer/features/auth/bloc/auth_event.dart';
+import 'package:home_organizer/features/chat/ui/chat_page.dart';
+import 'package:home_organizer/features/expenses/ui/expenses_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Home Organizer',
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        leading: Icon(Icons.wheelchair_pickup),
-        actions: [
-          IconButton(
-            onPressed: () => context.read<AuthBloc>().add(AuthEventLogOut()),
-            icon: Icon(Icons.logout),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Home Organizer',
+            style: Theme.of(context).textTheme.titleLarge,
           ),
-        ],
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          leading: Icon(Icons.wheelchair_pickup),
+          actions: [
+            IconButton(
+              onPressed: () => context.read<AuthBloc>().add(AuthEventLogOut()),
+              icon: Icon(Icons.logout),
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.message)),
+              Tab(icon: Icon(Icons.wallet)),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [ChatPage(), ExpensesPage()]),
       ),
-      body: Text('LOGGED'),
     );
   }
 }
