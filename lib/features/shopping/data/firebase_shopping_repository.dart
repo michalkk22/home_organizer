b11ed8981b68265fc2da22e56575d290f2fd8a7f
Michal Kolodziej <m.kolodziej2211@gmail.com>
2025-11-26 20:39:15 +0100

    implemented shopping repo

--- lib/features/shopping/data/firebase_shopping_repository.dart
+++ lib/features/shopping/data/firebase_shopping_repository.dart
@@ -1,3 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:home_organizer/constants/firebase_storage_constants.dart';
+import 'package:home_organizer/features/shopping/data/shopping_item.dart';
 import 'package:home_organizer/features/shopping/data/shopping_repository.dart';
+import 'package:home_organizer/features/shopping/domain/shopping_repository_exception.dart';
 
-class FirebaseShoppingRepository implements ShoppingRepository {}
+class FirebaseShoppingRepository implements ShoppingRepository {
+  final String _homeId;
+
+  FirebaseShoppingRepository(this._homeId);
+
+  late final _list = FirebaseFirestore.instance
+      .collection(HomesCollectionNames.collectionName)
+      .doc(_homeId)
+      .collection(ShoppingListCollectionNames.collectionName);
+
+  @override
+  Future<void> add(ShoppingItem item) async {
+    try {
+      await _list.add({
+        ShoppingListCollectionNames.nameFieldName: item.name,
+        ShoppingListCollectionNames.quantityFieldName: item.quantity,
+        ShoppingListCollectionNames.unitFieldName: item.unit,
+        ShoppingListCollectionNames.inCartFieldName: item.inCart,
+      });
+    } catch (e) {
+      throw CouldNotCreateShoppingRepositoryException();
+    }
+  }
+
+  @override
+  Future<void> delete(ShoppingItem item) async {
+    try {
+      await _list.doc(item.id).delete();
+    } catch (e) {
+      throw CouldNotDeleteShoppingRepositoryException();
+    }
+  }
+
+  @override
+  Stream<List<ShoppingItem>> getItems() => _list.snapshots().map(
+    (snapshot) =>
+        snapshot.docs.map((doc) => ShoppingItem.fromFirebase(doc)).toList(),
+  );
+
+  @override
+  Future<void> update(ShoppingItem item) async {
+    try {
+      await _list.doc(item.id).update({
+        ShoppingListCollectionNames.nameFieldName: item.name,
+        ShoppingListCollectionNames.quantityFieldName: item.quantity,
+        ShoppingListCollectionNames.unitFieldName: item.unit,
+        ShoppingListCollectionNames.inCartFieldName: item.inCart,
+      });
+    } catch (e) {
+      throw CouldNotDeleteShoppingRepositoryException();
+    }
+  }
+}
---
+++ lib/features/shopping/domain/shopping_repository_exception.dart
@@ -0,0 +1,7 @@
+class GenericShoppingRepositoryException implements Exception {}
+
+class CouldNotCreateShoppingRepositoryException implements Exception {}
+
+class CouldNotDeleteShoppingRepositoryException implements Exception {}
+
+class CouldNotUpdateShoppingRepositoryException implements Exception {}
